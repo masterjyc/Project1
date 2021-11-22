26f5aad1fe436efb60769d27d2d21ab64e4c3eb8
masterjyc <masterjinyc@163.com>
2021-11-22 22:45:13 +0800

    third modify

--- src/hello.java
+++ src/hello.java
@@ -4,8 +4,9 @@
  */
 public class hello {
     public static void main(String[] args) {
-        System.out.println("测试！！！");
 
         System.out.println("第二次修改");
+
+        System.out.println("第三次");
     }
 }
